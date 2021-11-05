fa1c4d485c8237d79d973ef80b2bef6bf1804a12
Andrei Gonçalves <andrei.goncalves@modalgr.com.br>
2021-11-05 01:20:03 -0300

    fix: s

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static('./dist/task'));
+app.use(express.static('./dist/tasks'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', { root: 'dist/task/' }),
+    res.sendFile('index.html', { root: 'dist/tasks/' }),
 );
 
 app.listen(process.env.PORT || 8080);
