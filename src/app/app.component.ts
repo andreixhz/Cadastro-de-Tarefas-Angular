8ef06a3851974fba6010581ec6b13e6e415ca0aa
Andrei Gonçalves <andrei.goncalves@modalgr.com.br>
2021-11-05 01:05:13 -0300

    feat: project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,98 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { NewTaskComponent } from './dialog/task/new-task/new-task.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'tasks';
+
+  tasks = {
+    progress: {
+      title: "Em andamento",
+      rows: [],
+    },
+    completed: {
+      title: "Concluídos",
+      rows: [],
+    },
+  }
+
+  loading: boolean = true;
+
+  constructor(
+    private http: HttpClient,
+    private dialogService: MatDialog
+  ) { }
+
+  ngOnInit(): void {
+
+    this.http.get('https://backend-tarefa-teteu.herokuapp.com/tasks/').subscribe((data: Task[]) => {
+      this.tasks.completed.rows = data.filter((task) => task.finished).sort((a, b) => a.id - b.id).reverse();
+      this.tasks.progress.rows = data.filter((task) => !task.finished).reverse();
+      this.loading = false;
+    })
+
+  }
+
+  handleFinalizateTask(task: Task) {
+
+    task.handing = true;
+
+    this.http.patch('https://backend-tarefa-teteu.herokuapp.com/tasks/' + task.id, {}).subscribe(
+      (response: any) => {
+
+        task.finished = true
+        this.tasks.progress.rows = this.tasks.progress.rows.filter((_task) => _task.id != task.id)
+        this.tasks.completed.rows.push(task)
+        this.tasks.completed.rows = this.tasks.completed.rows.sort((a, b) => a.id - b.id).reverse();
+
+
+      },
+      (error: any) => { },
+      () => { task.handing = false }
+    )
+
+  }
+
+  handleDelete(task: Task) {
+    task.handing = true;
+    this.http.delete('https://backend-tarefa-teteu.herokuapp.com/tasks/' + task.id).subscribe(
+      (response: any) => { this.tasks.progress.rows = this.tasks.progress.rows.filter((_task) => _task.id !== task.id) },
+      () => { },
+      () => { task.handing = false }
+    )
+  }
+
+  getSections() {
+    return Object.keys(this.tasks)
+
+  }
+
+  addNew() {
+    const dialogRef = this.dialogService.open(NewTaskComponent)
+    dialogRef.afterClosed().subscribe(result => {
+
+      if (result.id)
+        this.tasks.progress.rows.unshift(result)
+
+    });
+
+
+  }
+
+}
+
+interface Task {
+  id: number;
+  title: string;
+  description: string;
+  finished: boolean;
+  handing?: boolean;
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,37 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { HttpClientModule } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatDialogModule } from '@angular/material/dialog';
+import { NewTaskComponent } from './dialog/task/new-task/new-task.component';
+import { FormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewTaskComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    HttpClientModule,
+    MatCardModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatDialogModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
