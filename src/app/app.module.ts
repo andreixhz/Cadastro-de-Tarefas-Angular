dcff0f09adfb6bf7cb2e79a17678c9e2b65b58c3
Andrei Gonçalves <andrei.goncalves@modalgr.com.br>
2021-11-06 00:13:28 -0300

    feat: authetication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { MatCardModule } from '@angular/material/card';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
@@ -16,7 +16,9 @@ import { NewTaskComponent } from './dialog/task/new-task/new-task.component';
 import { FormsModule } from '@angular/forms'
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
-
+import { PagesModule } from './pages/pages.module';
+import { RouterModule } from '@angular/router';
+import { TokenInterceptor } from './interceptor/token.interceptor';
 @NgModule({
   declarations: [
     AppComponent,
@@ -36,9 +38,13 @@ import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
     MatDialogModule,
     FormsModule,
     DragDropModule,
-    NgxSkeletonLoaderModule
+    NgxSkeletonLoaderModule,
+    PagesModule,
+    RouterModule
   ],
-  providers: [],
+  providers: [
+    TokenInterceptor, { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true, },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
